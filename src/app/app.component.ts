28eaaa86c0aae691ed7ec67f0ea8ca7a391163ab
JPablos <jpablos2011.2@gmail.com>
2020-02-03 07:05:00 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Show, TvmazeService } from 'tvmaze';
+
+//import { IonButton } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  show$: Observable<Show>;
+
+  constructor(
+    private tvmaze: TvmazeService,
+    //private ionButton: IonButton
+  ) { }
+
   title = 'angular-libraries';
+  //
+  public innerWidth: any;
+
+  ngOnInit() {
+    this.show$ = this.tvmaze.getShow(336);
+
+    this.innerWidth = window.innerWidth;
+    console.log('ancho', this.innerWidth);
+  }
 }
+
+/*
+crear librerias
+
+https://www.competa.com/blog/angular-6-create-a-module-that-can-be-installed-with-npm/
+https://medium.com/@esanjiv/complete-beginner-guide-to-publish-an-angular-library-to-npm-d42343801660
+https://github.com/mrsan22/NgxMatTypeahead
+https://angular.io/guide/creating-libraries
+https://blog.angulartraining.com/create-your-own-libraries-with-angular-cli-7b434600bbb7
+
+https://medium.com/@tomsu/how-to-build-a-library-for-angular-apps-4f9b38b0ed11
+https://github.com/sulco/angular-lib-demo
+
+workspace
+https://devdactic.com/ionic-multi-app-shared-library/
+
+angular and ionic
+https://stackoverflow.com/questions/55330085/create-ionic-4-app-inside-angular-cli-projects-structure
+
+tamano de pantalla
+https://stackoverflow.com/questions/45350716/detect-window-size-using-angular-4
+https://scotch.io/tutorials/detect-responsive-screen-sizes-in-angular
+
+routing condicional
+https://stackoverflow.com/questions/34660263/angular2-conditional-routing
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TvmazeModule } from 'tvmaze';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+//import { SplashScreen } from '@ionic/angular/splash-screen/ngx';
+//import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { IonButton } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+import { RouteReuseStrategy } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TvmazeModule,
+    IonicModule.forRoot(),
+    //IonButton
   ],
-  providers: [],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    IonButton
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
